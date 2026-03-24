fdde9a354a4ad873d5ed3593a4b5d4caf9b63c58
Fernando Quinteros Gutierrez <ferkki@hotmail.com>
2026-03-23 23:22:54 -0400

    feat: add createBookmark function and schema validation for bookmark creation

--- src/actions/bookmark.ts
+++ src/actions/bookmark.ts
@@ -1,7 +1,21 @@
 'use server'
 
-import { pinBookmarkUseCase, trackVisitUseCase } from '@/core/container'
+import {
+  createBookmarkUseCase,
+  pinBookmarkUseCase,
+  trackVisitUseCase,
+} from '@/core/container'
+import type { CreateBookmarkInput } from '@/core/use-cases/bookmarks'
+import { createBookmarkSchema } from '@/features/bookmarks/schemas/bookmark-form.schema'
+
 import { revalidatePath } from 'next/cache'
+
+export const createBookmark = async (payload: CreateBookmarkInput) => {
+  const parsedPayload = createBookmarkSchema.parse(payload)
+
+  await createBookmarkUseCase.execute(parsedPayload)
+  revalidatePath('/')
+}
 
 export const trackBookmarkVisit = async (bookmarkId: string) => {
   await trackVisitUseCase.execute(bookmarkId)
