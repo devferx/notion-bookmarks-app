41000b92b7185d6a5b70e556e6f12a579e611550
Fernando Quinteros Gutierrez <ferkki@hotmail.com>
2026-03-30 23:29:20 -0400

    feat: add tags section to sidebar with dynamic checkboxes and styling

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -48,7 +48,7 @@ export const Sidebar = () => {
           </Link>
         </header>
 
-        <nav className="px-5" aria-label="Primary">
+        <nav className="px-4" aria-label="Primary">
           <Link
             className="flex items-center justify-start gap-2 rounded-md border border-transparent px-3 py-2 hover:border-neutral-100 hover:bg-neutral-100 dark:hover:border-transparent dark:hover:bg-neutral-600"
             href="/"
@@ -73,6 +73,77 @@ export const Sidebar = () => {
             </span>
           </Link>
         </nav>
+
+        <section className="flex-1 overflow-y-auto px-4 pb-5">
+          <header className="bg-neutral-0 sticky top-0 flex items-center justify-between px-3 py-2 dark:bg-neutral-800">
+            <h3 className="tags-title text-dark-gray uppercase">Tags</h3>
+            <button className="clear-tags-button cursor-pointer text-neutral-700 underline">
+              Reset
+            </button>
+          </header>
+
+          <ul>
+            <li className="flex items-center justify-between gap-2 px-3 py-2.5">
+              <input
+                className="h-4 w-4 cursor-pointer appearance-none rounded border-2 border-neutral-500 bg-center bg-no-repeat checked:border-teal-700 checked:bg-teal-700 checked:bg-[url('/checkmark.svg')]"
+                type="checkbox"
+                name="tag1"
+                id="tag1"
+              />
+              <label
+                className="text-preset-3 flex-1 text-neutral-800 select-none"
+                htmlFor="tag1"
+              >
+                AI
+              </label>
+
+              <span className="tags-counter flex h-6 w-6 items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 text-neutral-800">
+                1
+              </span>
+            </li>
+            <li className="flex items-center justify-between gap-2 px-3 py-2.5">
+              <input
+                className="h-4 w-4 cursor-pointer appearance-none rounded border-2 border-neutral-500 bg-center bg-no-repeat checked:border-teal-700 checked:bg-teal-700 checked:bg-[url('/checkmark.svg')]"
+                type="checkbox"
+                name="tag2"
+                id="tag2"
+              />
+              <label
+                className="text-preset-3 flex-1 text-neutral-800 select-none"
+                htmlFor="tag2"
+              >
+                Commynity
+              </label>
+
+              <span className="tags-counter flex h-6 w-6 items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 text-neutral-800">
+                5
+              </span>
+            </li>
+            {new Array(20).fill(null).map((_, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-between gap-2 px-3 py-2.5"
+              >
+                <input
+                  className="h-4 w-4 cursor-pointer appearance-none rounded border-2 border-neutral-500 bg-center bg-no-repeat checked:border-teal-700 checked:bg-teal-700 checked:bg-[url('/checkmark.svg')]"
+                  type="checkbox"
+                  name={`tag${index + 3}`}
+                  id={`tag${index + 3}`}
+                />
+                <label
+                  className="text-preset-3 flex-1 text-neutral-800 select-none"
+                  htmlFor={`tag${index + 3}`}
+                >
+                  Tag {index + 3}
+                </label>
+
+                <span className="tags-counter flex h-6 w-6 items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 text-neutral-800">
+                  {Math.floor(Math.random() * 10) + 1}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </section>
       </aside>
 
       {isSidebarOpen && (
