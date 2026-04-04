b77cbe70c17b8d3dcc29d6614fad6bdf7f4be8ac
Fernando Quinteros Gutierrez <ferkki@hotmail.com>
2026-04-03 23:14:05 -0400

    feat: enhance tag filter component with custom scrollbar and dark mode styles

--- src/features/bookmarks/components/tag-filter-selector.tsx
+++ src/features/bookmarks/components/tag-filter-selector.tsx
@@ -12,11 +12,13 @@ export const TagFilterSelector = ({ tags, selectedTags }: TagListProps) => {
   const { onToggleTag, onResetTags } = useTagFilter()
 
   return (
-    <section className="flex-1 overflow-y-auto px-4 pb-5">
+    <section className="bookmark-tags-scrollbar flex-1 overflow-y-auto px-4 pb-5">
       <header className="bg-neutral-0 sticky top-0 flex items-center justify-between px-3 py-2 dark:bg-neutral-800">
-        <h3 className="tags-title text-dark-gray uppercase">Tags</h3>
+        <h3 className="tags-title text-dark-gray uppercase dark:text-neutral-100">
+          Tags
+        </h3>
         <button
-          className="clear-tags-button cursor-pointer text-neutral-700 underline"
+          className="clear-tags-button cursor-pointer text-neutral-700 underline dark:text-neutral-100"
           onClick={onResetTags}
         >
           Reset
@@ -38,13 +40,13 @@ export const TagFilterSelector = ({ tags, selectedTags }: TagListProps) => {
               onChange={() => onToggleTag(tag.name)}
             />
             <label
-              className="text-preset-3 flex-1 text-neutral-800 select-none"
+              className="text-preset-3 flex-1 cursor-pointer text-neutral-800 select-none dark:text-neutral-100"
               htmlFor={`tag-${tag.name}`}
             >
               {tag.name}
             </label>
 
-            <span className="tags-counter flex h-6 w-6 items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 text-neutral-800">
+            <span className="tags-counter dark:text-neutral-0 flex h-6 w-6 items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 text-center text-neutral-800 dark:border-neutral-400 dark:bg-neutral-600">
               {tag.count}
             </span>
           </li>
