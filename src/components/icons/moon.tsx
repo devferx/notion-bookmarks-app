944cbf8e89147c9744d2268440ae9bad52a1487d
Fernando Quinteros Gutierrez <ferkki@hotmail.com>
2026-04-05 00:21:38 -0400

    feat: add new icon components for logout, moon, palette, and sun

---
+++ src/components/icons/moon.tsx
@@ -0,0 +1,31 @@
+import type { SVGProps } from 'react'
+
+type Props = SVGProps<SVGSVGElement> & {
+  size?: number
+}
+
+export const Moon = ({ size = 16, ...props }: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 14 14"
+    fill="none"
+    width={size}
+    height={size}
+    {...props}
+  >
+    <g clipPath="url(#clip0_682_890)">
+      <path
+        d="M12.8072 7.55813C12.004 8.96705 10.488 9.91696 8.7502 9.91696C6.17287 9.91696 4.08354 7.82762 4.08354 5.25029C4.08354 3.51233 5.0336 1.99626 6.44269 1.19312C3.48255 1.47378 1.16687 3.96654 1.16687 7.00015C1.16687 10.2218 3.77854 12.8335 7.0002 12.8335C10.0337 12.8335 12.5263 10.518 12.8072 7.55813Z"
+        stroke="currentColor"
+        strokeWidth={1.3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_682_890">
+        <rect width={14} height={14} fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+)
