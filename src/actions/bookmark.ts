a5f8ccee97ac15e3a7be7d2b533359e5e2595f32
Fernando Quinteros Gutierrez <ferkki@hotmail.com>
2026-04-07 22:37:57 -0400

    feat: add setBookmarkArchive and deleteBookmark functions with cache revalidation

--- src/actions/bookmark.ts
+++ src/actions/bookmark.ts
@@ -5,7 +5,9 @@ import { revalidatePath, revalidateTag } from 'next/cache'
 import { BOOKMARK_CACHE_TAGS } from '@/core/constants/bookmark'
 import {
   createBookmarkUseCase,
+  deleteBookmarkUseCase,
   pinBookmarkUseCase,
+  updateBookmarkArchiveStatusUseCase,
   trackVisitUseCase,
 } from '@/core/container'
 import type { CreateBookmarkInput } from '@/core/use-cases/bookmarks'
@@ -23,11 +25,36 @@ export const createBookmark = async (payload: CreateBookmarkInput) => {
 export const trackBookmarkVisit = async (bookmarkId: string) => {
   await trackVisitUseCase.execute(bookmarkId)
   revalidateTag(BOOKMARK_CACHE_TAGS.bookmarksList, 'max')
+  revalidateTag(BOOKMARK_CACHE_TAGS.archivedBookmarksList, 'max')
   revalidatePath('/')
+  revalidatePath('/archived')
 }
 
 export const setBookmarkPin = async (bookmarkId: string, isPinned: boolean) => {
   await pinBookmarkUseCase.execute(bookmarkId, isPinned)
   revalidateTag(BOOKMARK_CACHE_TAGS.bookmarksList, 'max')
+  revalidateTag(BOOKMARK_CACHE_TAGS.archivedBookmarksList, 'max')
   revalidatePath('/')
+  revalidatePath('/archived')
+}
+
+export const setBookmarkArchive = async (
+  bookmarkId: string,
+  isArchived: boolean,
+) => {
+  await updateBookmarkArchiveStatusUseCase.execute(bookmarkId, isArchived)
+  revalidateTag(BOOKMARK_CACHE_TAGS.bookmarksList, 'max')
+  revalidateTag(BOOKMARK_CACHE_TAGS.bookmarksTags, 'max')
+  revalidateTag(BOOKMARK_CACHE_TAGS.archivedBookmarksList, 'max')
+  revalidatePath('/')
+  revalidatePath('/archived')
+}
+
+export const deleteBookmark = async (bookmarkId: string) => {
+  await deleteBookmarkUseCase.execute(bookmarkId)
+  revalidateTag(BOOKMARK_CACHE_TAGS.bookmarksList, 'max')
+  revalidateTag(BOOKMARK_CACHE_TAGS.bookmarksTags, 'max')
+  revalidateTag(BOOKMARK_CACHE_TAGS.archivedBookmarksList, 'max')
+  revalidatePath('/')
+  revalidatePath('/archived')
 }
