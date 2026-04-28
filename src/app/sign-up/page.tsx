08e03a2a87a7fb9d4f1e183f3e8f8c3b1d3fd047
Fernando Quinteros Gutierrez <ferkki@hotmail.com>
2026-04-27 23:17:14 -0400

    feat: replace inline sign-in and sign-up forms with dedicated components

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,6 +1,8 @@
 import { Logo } from '@/components/icons'
 import Link from 'next/link'
 
+import { SignUpForm } from '@/features/auth/components/sign-up-form'
+
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-neutral-100 px-4 dark:bg-neutral-900">
@@ -22,58 +24,7 @@ export default function SignInPage() {
           </p>
         </div>
 
-        <form className="flex flex-col gap-6">
-          <div className="grid gap-1.5">
-            <label
-              className="text-preset-4 dark:text-neutral-0 text-neutral-900"
-              htmlFor="name"
-            >
-              Full name
-              <span className="text-teal-70 dark:text-neutral-100">*</span>
-            </label>
-            <input
-              className="bg-neutral-0 text-input-shadow rounded-lg border border-neutral-500 p-3 dark:border-neutral-300 dark:bg-neutral-600"
-              type="text"
-              id="fullname"
-              name="fullname"
-            />
-          </div>
-          <div className="grid gap-1.5">
-            <label
-              className="text-preset-4 dark:text-neutral-0 text-neutral-900"
-              htmlFor="email"
-            >
-              Email address
-              <span className="text-teal-70 dark:text-neutral-100">*</span>
-            </label>
-            <input
-              className="bg-neutral-0 text-input-shadow rounded-lg border border-neutral-500 p-3 dark:border-neutral-300 dark:bg-neutral-600"
-              type="email"
-              id="email"
-            />
-          </div>
-          <div className="grid gap-1.5">
-            <label
-              className="text-preset-4 dark:text-neutral-0 text-neutral-900"
-              htmlFor="password"
-            >
-              Password
-              <span className="text-teal-70 dark:text-neutral-100">*</span>
-            </label>
-            <input
-              className="bg-neutral-0 text-input-shadow rounded-lg border border-neutral-500 p-3 dark:border-neutral-300 dark:bg-neutral-600"
-              type="password"
-              id="password"
-            />
-          </div>
-
-          <button
-            className="button-shadow text-preset-3 text-neutral-0 cursor-pointer rounded-lg bg-teal-700 px-4 py-3"
-            type="submit"
-          >
-            Create account
-          </button>
-        </form>
+        <SignUpForm />
 
         <div className="flex flex-col items-center gap-3">
           <p className="text-preset-4-medium flex items-center gap-1.5 text-neutral-800 dark:text-neutral-100">
